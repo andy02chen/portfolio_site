0f54124ae0b7b5d44dacbc0e1d51cd88e1281501
Andy Chen <andy02chen@gmail.com>
2025-10-06 13:19:57 +1100

    added animations to cross, hamburger and collapsing

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState, type SetStateAction } from "react";
 
 function App() {
 
   const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
   const [nav, setNav] = useState('home');
+
+  const [showHamburger, setShowHamburger] = useState(true);
+  const [showCross, setShowCross] = useState(false);
+  const [animatingHamburger, setAnimatingHamburger] = useState(false);
+  const [animatingCross, setAnimatingCross] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setAnimatingHamburger(true);
+    setShowCross(true);
+    setTimeout(() => {
+      setAnimatingCross(true);
+      setShowHamburgerMenu(true);
+    }, 50);
+    setTimeout(() => {
+      setShowHamburger(false);
+    });
+  };
+
+  const handleCrossClick = () => {
+    setShowHamburgerMenu(false);
+    setTimeout(() => {
+      setAnimatingCross(false);
+      setShowHamburger(true);
+      setTimeout(() => {
+        setAnimatingHamburger(false);
+      }, 50);
+      setTimeout(() => {
+        setShowCross(false);
+      });
+    });
+  };
+
+  const handleNavClick = (link: SetStateAction<string>) => {
+    setNav(link);
+    handleCrossClick();
+  };
 
   return (
     <main className="min-h-dvh w-full bg-[var(--dark-blue)] text-[var(--white)]">
@@ -13,67 +49,61 @@ function App() {
         <div className={`lg:hidden w-full
           ${showHamburgerMenu? "border-t border-l border-r border-[var(--light-yellow)]" : ""}`}>
           
-          {/* Menu Button */}
-          {/* <button className={`transition-all duration-500 ease-in-out
-          ${!showHamburgerMenu ? 
-          'w-10 h-10 m-4 opacity-100 translate-y-0' : 
-          "max-h-0 opacity-0 -translate-y-4 overflow-hidden pointer-events-none"}`}
-            onClick={() => setShowHamburgerMenu(true)}
-          >
-            <svg className="w-full h-full" viewBox="0 0 334 334" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0V72.6087H298.445L334 3.84695e-06L0 0Z" fill="white"/>
-            <path d="M0 261.391V334H237.744L266.068 261.391L0 261.391Z" fill="white"/>
-            <path d="M0 130.696V203.304H268.095L300.034 130.696L0 130.696Z" fill="#FFFF33"/>
-            </svg>
-          </button> */}
-          
-          {!showHamburgerMenu &&
-            <button className="w-10 h-10 m-4"
-              onClick={() => setShowHamburgerMenu(true)}
-            >
-              <svg className="w-full h-full" viewBox="0 0 334 334" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0V72.6087H298.445L334 3.84695e-06L0 0Z" fill="white"/>
-              <path d="M0 261.391V334H237.744L266.068 261.391L0 261.391Z" fill="white"/>
-              <path d="M0 130.696V203.304H268.095L300.034 130.696L0 130.696Z" fill="#FFFF33"/>
-              </svg>
-            </button>
-          }
+          <div className="relative w-10 h-10 m-4">
+            {/* Menu Button */}
+            {showHamburger &&
+              <button className={`absolute top-0 left-0 w-10 h-10 transition-all duration-500
+              ${animatingHamburger ? 
+              'opacity-0 rotate-180' : 
+              'opacity-100 rotate-0'}`}
+                onClick={handleHamburgerClick}
+              >
+                <svg className="w-full h-full" viewBox="0 0 334 334" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0V72.6087H298.445L334 3.84695e-06L0 0Z" fill="white"/>
+                <path d="M0 261.391V334H237.744L266.068 261.391L0 261.391Z" fill="white"/>
+                <path d="M0 130.696V203.304H268.095L300.034 130.696L0 130.696Z" fill="#FFFF33"/>
+                </svg>
+              </button>
+            }
+
+            {/* Cross Button */}
+            {showCross &&
+              <button 
+                className={`absolute top-0 left-0 w-10 h-10 transition-all duration-500
+                  ${animatingCross 
+                    ? 'opacity-100 rotate-0' 
+                    : 'opacity-0 -rotate-180'
+                  }`}
+                onClick={handleCrossClick}
+              >
+                <svg className="h-full w-full" viewBox="0 0 334 335" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M166.667 214.14L56.9036 323.904C43.8861 336.921 22.7806 336.921 9.76311 323.904C-3.25437 310.886 -3.25437 289.781 9.76311 276.763L119.526 167L9.76311 57.2369C-3.25437 44.2195 -3.25437 23.114 9.76311 10.0965C22.7806 -2.92099 43.8861 -2.92099 56.9036 10.0965L166.667 119.86L276.43 10.0965C289.447 -2.92099 310.553 -2.92099 323.57 10.0965C336.588 23.114 336.588 44.2195 323.57 57.2369L213.807 167L323.57 276.763C336.588 289.781 336.588 310.886 323.57 323.904C310.553 336.921 289.447 336.921 276.43 323.904L166.667 214.14Z" fill="#323232"/>
+                  <mask id="mask0_217_20" style={{maskType:"luminance"}} maskUnits="userSpaceOnUse" x="0" y="0" width="334" height="334">
+                  <path d="M166.667 214.14L56.9036 323.904C43.8861 336.921 22.7806 336.921 9.76311 323.904C-3.25437 310.886 -3.25437 289.781 9.76311 276.763L119.526 167L9.76311 57.2369C-3.25437 44.2195 -3.25437 23.114 9.76311 10.0965C22.7806 -2.92099 43.8861 -2.92099 56.9036 10.0965L166.667 119.86L276.43 10.0965C289.447 -2.92099 310.553 -2.92099 323.57 10.0965C336.588 23.114 336.588 44.2195 323.57 57.2369L213.807 167L323.57 276.763C336.588 289.781 336.588 310.886 323.57 323.904C310.553 336.921 289.447 336.921 276.43 323.904L166.667 214.14Z" fill="white"/>
+                  </mask>
+                  <g mask="url(#mask0_217_20)">
+                  <path d="M-233.334 -233L-233.334 567H566.666V-233H-233.334Z" fill="white"/>
+                  </g>
+                  <path d="M9.76311 57.9036C-3.25437 44.8861 -3.25437 23.7806 9.76311 10.7631C22.7806 -2.25437 43.8861 -2.25437 56.9036 10.7631L323.57 277.43C336.588 290.447 336.588 311.553 323.57 324.57C310.553 337.588 289.447 337.588 276.43 324.57L9.76311 57.9036Z" fill="#D8D8D8"/>
+                  <mask id="mask1_217_20" style={{maskType:"luminance"}} maskUnits="userSpaceOnUse" x="0" y="1" width="334" height="334">
+                  <path d="M9.76311 57.9036C-3.25437 44.8861 -3.25437 23.7806 9.76311 10.7631C22.7806 -2.25437 43.8861 -2.25437 56.9036 10.7631L323.57 277.43C336.588 290.447 336.588 311.553 323.57 324.57C310.553 337.588 289.447 337.588 276.43 324.57L9.76311 57.9036Z" fill="white"/>
+                  </mask>
+                  <g mask="url(#mask1_217_20)">
+                  <path d="M533.334 -199H-266.666V601H533.334V-199Z" fill="#FFFF33"/>
+                  </g>
+                </svg>
+              </button>
+            }
+          </div>
 
           {/* Menu */}
-            <div className={`w-full bg-[var(--dark-blue)] flex flex-col 
-            transition-all duration-500 ease-in-out
-            ${showHamburgerMenu 
-              ? 'max-h-screen opacity-100 translate-y-0' 
-              : 'max-h-0 opacity-0 -translate-y-4 overflow-hidden pointer-events-none'
-            }`}>
-            {/* Cross Button */}
-            <button 
-              className={`w-10 h-10 m-4 transition-all duration-700
-                ${showHamburgerMenu 
-                  ? 'opacity-100 rotate-0' 
-                  : 'opacity-0 rotate-90 pointer-events-none'
-                }`}
-              onClick={() => setShowHamburgerMenu(false)}
-            >
-              <svg className="h-full w-full" viewBox="0 0 334 335" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M166.667 214.14L56.9036 323.904C43.8861 336.921 22.7806 336.921 9.76311 323.904C-3.25437 310.886 -3.25437 289.781 9.76311 276.763L119.526 167L9.76311 57.2369C-3.25437 44.2195 -3.25437 23.114 9.76311 10.0965C22.7806 -2.92099 43.8861 -2.92099 56.9036 10.0965L166.667 119.86L276.43 10.0965C289.447 -2.92099 310.553 -2.92099 323.57 10.0965C336.588 23.114 336.588 44.2195 323.57 57.2369L213.807 167L323.57 276.763C336.588 289.781 336.588 310.886 323.57 323.904C310.553 336.921 289.447 336.921 276.43 323.904L166.667 214.14Z" fill="#323232"/>
-                <mask id="mask0_217_20" style={{maskType:"luminance"}} maskUnits="userSpaceOnUse" x="0" y="0" width="334" height="334">
-                <path d="M166.667 214.14L56.9036 323.904C43.8861 336.921 22.7806 336.921 9.76311 323.904C-3.25437 310.886 -3.25437 289.781 9.76311 276.763L119.526 167L9.76311 57.2369C-3.25437 44.2195 -3.25437 23.114 9.76311 10.0965C22.7806 -2.92099 43.8861 -2.92099 56.9036 10.0965L166.667 119.86L276.43 10.0965C289.447 -2.92099 310.553 -2.92099 323.57 10.0965C336.588 23.114 336.588 44.2195 323.57 57.2369L213.807 167L323.57 276.763C336.588 289.781 336.588 310.886 323.57 323.904C310.553 336.921 289.447 336.921 276.43 323.904L166.667 214.14Z" fill="white"/>
-                </mask>
-                <g mask="url(#mask0_217_20)">
-                <path d="M-233.334 -233L-233.334 567H566.666V-233H-233.334Z" fill="white"/>
-                </g>
-                <path d="M9.76311 57.9036C-3.25437 44.8861 -3.25437 23.7806 9.76311 10.7631C22.7806 -2.25437 43.8861 -2.25437 56.9036 10.7631L323.57 277.43C336.588 290.447 336.588 311.553 323.57 324.57C310.553 337.588 289.447 337.588 276.43 324.57L9.76311 57.9036Z" fill="#D8D8D8"/>
-                <mask id="mask1_217_20" style={{maskType:"luminance"}} maskUnits="userSpaceOnUse" x="0" y="1" width="334" height="334">
-                <path d="M9.76311 57.9036C-3.25437 44.8861 -3.25437 23.7806 9.76311 10.7631C22.7806 -2.25437 43.8861 -2.25437 56.9036 10.7631L323.57 277.43C336.588 290.447 336.588 311.553 323.57 324.57C310.553 337.588 289.447 337.588 276.43 324.57L9.76311 57.9036Z" fill="white"/>
-                </mask>
-                <g mask="url(#mask1_217_20)">
-                <path d="M533.334 -199H-266.666V601H533.334V-199Z" fill="#FFFF33"/>
-                </g>
-              </svg>
-            </button>
-
-            <ul className="flex flex-col text-3xl text-[var(--white)] mt-4">
+          <div className={`w-full bg-[var(--dark-blue)] flex flex-col 
+          transition-all duration-500 ease-in-out
+          ${showHamburgerMenu 
+            ? 'max-h-screen opacity-100 translate-y-0' 
+            : 'max-h-0 opacity-0 -translate-y-4 overflow-hidden'
+          }`}>
+            <ul className="flex flex-col text-3xl text-[var(--white)]">
               <li className={`w-full border-l-[5px] overflow-hidden text-ellipsis whitespace-nowrap
                 ${ nav === 'home'? "bg-[var(--light-blue)] border-[var(--light-yellow)]" : 
                 "bg-[var(--dark-blue)] border-transparent" }`}>
@@ -81,7 +111,7 @@ function App() {
                   href="#home" 
                   draggable="false" 
                   className="flex flex-row items-center gap-4 py-5 px-6"
-                  onClick={() => {setNav('home'), setShowHamburgerMenu(false)}}
+                  onClick={() => handleNavClick('home')}
                 >
                   <div className="h-12 w-12 flex-shrink-0">
                     <svg className="h-full w-full" viewBox="0 0 291 326" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -99,7 +129,7 @@ function App() {
                   href="#about_me" 
                   draggable="false" 
                   className="flex flex-row items-center gap-4 py-5 px-6"
-                  onClick={() => {setNav('about_me'), setShowHamburgerMenu(false)}}
+                  onClick={() => handleNavClick('about_me')}
                 >
                   <div className="h-12 w-12 flex-shrink-0">
                     <svg className="h-full w-full" viewBox="0 0 63 64" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -119,7 +149,7 @@ function App() {
                   href="#projects" 
                   draggable="false" 
                   className="flex flex-row items-center gap-4 py-5 px-6"
-                  onClick={() => {setNav('projects'), setShowHamburgerMenu(false)}}
+                  onClick={() => handleNavClick('projects')}
                 >
                   <div className="h-12 w-12 flex-shrink-0">
                     <svg className="h-full w-full" viewBox="0 0 70 64" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -137,7 +167,7 @@ function App() {
                   href="#contact" 
                   draggable="false" 
                   className="flex flex-row items-center gap-4 py-5 px-6"
-                  onClick={() => {setNav('contact'), setShowHamburgerMenu(false)}}
+                  onClick={() => handleNavClick('contact')}
                 >
                   <div className="h-12 w-12 flex-shrink-0">
                     <svg className="h-full w-full" viewBox="0 0 53 67" fill="none" xmlns="http://www.w3.org/2000/svg">
