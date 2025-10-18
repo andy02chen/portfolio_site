4c44b37e3b838f52f71f7d8a9b825320a47baa58
Andy Chen <andy02chen@gmail.com>
2025-10-18 12:44:06 +1100

    changes to text size on larger displays

--- portfolio/src/About.tsx
+++ portfolio/src/About.tsx
@@ -9,15 +9,15 @@ export default function About({ setNav }: HomeProps) {
   return(
     <section id="about_me" className="w-full h-[calc(100vh-73px)] lg:h-[calc(100vh-64px)]
     flex items-center justify-center">
-      <div className="w-8/10 h-8/10 bg-[var(--light-blue)] border-1 border-[var(--yellow)]
+      <div className="w-9/10 h-9/10 bg-[var(--light-blue)] border-1 border-[var(--yellow)]
       flex flex-col">
 
         {/* Top Div */}
-        <div className='w-1/2 h-1/8'>
+        <div className='w-full h-auto'>
           <div className="grid grid-cols-[4rem_auto_4rem] justify-stretch ...">
             <div></div>
             <div>
-              <h1 className="font-furore font-bold text-4xl sm:text-6xl lg:text-7xl px-4 w-full flex ">
+              <h1 className="font-furore font-bold text-4xl sm:text-6xl md:text-5xl xl:text-6xl 2xl:text-7xl px-4 w-full flex ">
                 About me __
               </h1>
             </div>
@@ -26,13 +26,13 @@ export default function About({ setNav }: HomeProps) {
         </div>
 
         {/* Buttom Div */}
-        <div className='flex flex-row w-full h-full'>
+        <div className='flex flex-col 2xl:flex-row w-full h-full'>
 
           {/* Left Div */}
           <div className="flex-auto flex flex-col">
             
             <div className="flex h-full items-center justify-center">
-              <svg className='w-9/10 h-auto' viewBox="0 0 1101 664" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg className='w-8/10 h-auto' viewBox="0 0 1101 664" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <defs>
                   <clipPath id="aboutMeClip">
                     <rect x="78.5" y="25.5" width="1021" height="575" stroke="#FFD700" stroke-width="3"/>
@@ -59,7 +59,7 @@ export default function About({ setNav }: HomeProps) {
           {/* Right Div */}
           <div className="flex-1 h-full flex justify-center items-center">
             <div className='bg-[var(--dark-blue)] h-auto w-9/10 p-4'>
-              <p className='font-ubuntu text-2xl '>
+              <p className='font-ubuntu text-sm xl:text-lg 2xl:text-xl 3xl:text-2xl'>
                 Hi, I'm Andy Chen, a Computing graduate from the University of Sydney, majoring in Computer Science.
                 <br/><br/>
                 I currently work as a Junior Solution Engineer, where I help manage customer IT infrastructure, configure endpoint 
